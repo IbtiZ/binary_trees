3a983a1b97c7ead132978762557efc975e699841
IbtiZ <ibtissamzoubir@gmail.com>
2023-10-05 08:06:17 -0700

    134-main.c

--- tests/134-main.c
+++ tests/134-main.c
@@ -35,17 +35,4 @@ int main(void)
 		79, 47, 68, 87, 84, 91, 21, 32, 34, 2,
 		20, 22, 98, 1, 62, 95
 	};
-	size_t n = sizeof(array) / sizeof(array[0]);
-	int *sorted;
-	size_t sorted_size;
-
-	print_array(array, n);
-	tree = array_to_heap(array, n);
-	if (!tree)
-		return (1);
-	binary_tree_print(tree);
-	sorted = heap_to_sorted_array(tree, &sorted_size);
-	print_array(sorted, sorted_size);
-	free(sorted);
-	return (0);
-}
+	size_t n = sizeof(array) / sizeof(array
